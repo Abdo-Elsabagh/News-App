e1681dba1d206de94768e13a5e8dc80e23bd29a3
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-02-19 01:47:54 +0200

    add textFormField it's password

--- lib/feature/auth/login_view.dart
+++ lib/feature/auth/login_view.dart
@@ -14,25 +14,52 @@ class _LoginViewState extends State<LoginView> {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/logo.png',
-                width: 250,
-              ),
-              Text('Login to your Account',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.white)),
-              const SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                decoration: InputDecoration(),
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 250,
+                ),
+                Text('Login to your Account',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.white)),
+                const SizedBox(
+                  height: 40,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: AppColors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.email),
+                      label: const Text('Email'),
+                      hintText: ('Enter y Email'),
+                      hintStyle:
+                          TextStyle(color: AppColors.white, fontSize: 15)),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                    style: TextStyle(color: AppColors.white),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.remove_red_eye),
+                          onPressed: () {},
+                        ),
+                        label: const Text('Password'),
+                        hintText: ('Enter y Password'),
+                        hintStyle:
+                            TextStyle(color: AppColors.white, fontSize: 15))),
+              ],
+            ),
           ),
         ),
       ),
