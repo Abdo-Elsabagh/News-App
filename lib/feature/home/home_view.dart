4c2f685a3b9a949eac138c4b049fb261815dec49
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-02-29 00:32:13 +0200

    Add bottom navigation bar with icons and screens add screens

--- lib/feature/home/home_view.dart
+++ lib/feature/home/home_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:insights_news/core/app_colors.dart';
+import 'package:insights_news/feature/news/news_views.dart';
+import 'package:insights_news/feature/notifications/notification_view.dart';
+import 'package:insights_news/feature/profile/profile_viem.dart';
+import 'package:insights_news/feature/search/search_views.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -9,16 +14,36 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   @override
+  int currentIndex = 0;
+  List<Widget> screens = [
+    const NewsView(),
+    const SearchView(),
+    const NotificationView(),
+    const ProfileView(),
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Column(),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.notifications), label: 'Notifications'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-      ]),
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: AppColors.lomanda,
+          unselectedItemColor: AppColors.grey,
+          backgroundColor: AppColors.sacffoldBG,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: 'Notifications'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ]),
     );
   }
 }
