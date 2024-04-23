a6bf87ba376fa0a67ed3a046172bae49f77c3c47
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-04-23 03:30:27 +0200

    Update NewsView with new layout and add carousel_slider dependency
    The NewsView widget has been updated with a new layout that includes a greeting, user profile picture, and a padding. Additionally, the carousel_slider package has been

--- lib/feature/news/news_views.dart
+++ lib/feature/news/news_views.dart
@@ -11,13 +11,46 @@ class NewsView extends StatefulWidget {
 class _NewsViewState extends State<NewsView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'News',
-          style: TextStyle(color: AppColors.lomanda),
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello Sayed',
+                      style: TextStyle(color: AppColors.white, fontSize: 14),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Have a nice day',
+                      style: TextStyle(
+                        color: AppColors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const CircleAvatar(
+                  radius: 22,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/image 2.jpg'),
+                    radius: 20,
+                  ),
+                )
+              ],
+            )
+          ],
         ),
-      ),
+      )),
     );
   }
 }
