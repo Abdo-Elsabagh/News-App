f3a364b44230c11d14e0d3088492b4423ac084c5
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-07-02 01:27:39 +0300

    "Updated SearchView UI with AppBar, TextField, and ListView; modified pubspec.lock with version changes for several dependencies."

--- lib/feature/search/search_views.dart
+++ lib/feature/search/search_views.dart
@@ -11,13 +11,110 @@ class SearchView extends StatefulWidget {
 class _SearchViewState extends State<SearchView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Search',
-          style: TextStyle(color: AppColors.lomanda),
-        ),
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.sacffoldBG,
+            centerTitle: true,
+            title: const Text(
+              'Find Your News',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                style: TextStyle(color: AppColors.white),
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xff4F4F4F),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: AppColors.grey)),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: AppColors.white,
+                    ),
+                    label: const Text('Search for news'),
+                    labelStyle:
+                        TextStyle(fontSize: 18, color: AppColors.white)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10.0, left: 2, right: 10, bottom: 10),
+                child: Text(
+                  'Total Results : 10',
+                  style: TextStyle(color: AppColors.white, fontSize: 16),
+                ),
+              ),
+              Expanded(
+                  child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: AppColors.containerBG,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  'assets/image 2.jpg',
+                                  width: 160,
+                                  height: 100,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text(
+                                    'Man City stay perfect despite Rodri red against Forest',
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Image.asset('assets/read.png'),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        'READ',
+                                        style:
+                                            TextStyle(color: AppColors.white),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ))
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(
+                          height: 10,
+                        );
+                      },
+                      itemCount: 4))
+            ],
+          )),
     );
   }
 }
