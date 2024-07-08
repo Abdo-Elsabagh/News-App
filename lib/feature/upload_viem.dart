15463af7d5df85ead2f3fd4b40e643e910e05639
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-07-09 01:28:03 +0300

    add snackbar of uploadViem

--- lib/feature/upload_viem.dart
+++ lib/feature/upload_viem.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:insights_news/core/app_colors.dart';
 import 'package:insights_news/core/app_local_storage.dart';
+import 'package:insights_news/feature/home/home_view.dart';
 
 String? imagePath;
+String? name;
 
 class UploadViem extends StatefulWidget {
   const UploadViem({super.key});
@@ -32,7 +34,26 @@ class _UploadViemState extends State<UploadViem> {
       appBar: AppBar(
         actions: [
           TextButton(
-              onPressed: () {},
+              onPressed: () {
+                if (imagePath != null && name!.isNotEmpty) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const HomeView(),
+                  ));
+                } else if (imagePath == null && name!.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      backgroundColor: Colors.red,
+                      content:
+                          Text('Please Upload Image and Enter Your Name')));
+                } else if (imagePath == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text('Please Upload Image')));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text('Please Enter Your Name')));
+                }
+              },
               child: Text(
                 'Done',
                 style: TextStyle(color: AppColors.lomanda),
@@ -40,83 +61,81 @@ class _UploadViemState extends State<UploadViem> {
         ],
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                    radius: 60,
-                    backgroundColor: AppColors.grey,
-                    backgroundImage: (imagePath != null)
-                        ? FileImage(File(imagePath!)) as ImageProvider
-                        : const AssetImage('assets/user.png')),
-                const SizedBox(
-                  height: 20,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    getImagrFormCamera();
-                  },
-                  child: Container(
-                    height: 40,
-                    width: 200,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: AppColors.containerBG),
-                    child: Text(
-                      'Uplaod for camera',
-                      style: TextStyle(
-                          color: AppColors.lomanda,
-                          fontWeight: FontWeight.w700),
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                  radius: 60,
+                  backgroundColor: AppColors.grey,
+                  backgroundImage: (imagePath != null)
+                      ? FileImage(File(imagePath!)) as ImageProvider
+                      : const AssetImage('assets/user.png')),
+              const SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  getImagrFormCamera();
+                },
+                child: Container(
+                  height: 40,
+                  width: 200,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.containerBG),
+                  child: Text(
+                    'Uplaod for camera',
+                    style: TextStyle(
+                        color: AppColors.lomanda, fontWeight: FontWeight.w700),
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    getImagrFormGallery();
-                  },
-                  child: Container(
-                    height: 40,
-                    width: 200,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: AppColors.containerBG),
-                    child: Text(
-                      'Uplaod for Gallery',
-                      style: TextStyle(
-                          color: AppColors.lomanda,
-                          fontWeight: FontWeight.w700),
-                    ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () {
+                  getImagrFormGallery();
+                },
+                child: Container(
+                  height: 40,
+                  width: 200,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.containerBG),
+                  child: Text(
+                    'Uplaod for Gallery',
+                    style: TextStyle(
+                        color: AppColors.lomanda, fontWeight: FontWeight.w700),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: Divider(
-                    color: AppColors.lomanda,
-                    height: 40,
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Divider(
+                  color: AppColors.lomanda,
+                  height: 40,
                 ),
-                TextFormField(
-                  cursorColor: AppColors.lomanda,
-                  onChanged: (value) {
-                    setState(() {});
-                  },
-                  style: TextStyle(color: AppColors.white),
-                  decoration: InputDecoration(
-                      hintText: 'Enter Your Name',
-                      hintStyle: TextStyle(color: AppColors.grey),
-                      filled: true,
-                      fillColor: AppColors.containerBG),
-                )
-              ],
-            ),
+              ),
+              TextFormField(
+                cursorColor: AppColors.lomanda,
+                onChanged: (value) {
+                  setState(() {
+                    name = value;
+                  });
+                },
+                style: TextStyle(color: AppColors.white),
+                decoration: InputDecoration(
+                    hintText: 'Enter Your Name',
+                    hintStyle: TextStyle(color: AppColors.grey),
+                    filled: true,
+                    fillColor: AppColors.containerBG),
+              )
+            ],
           ),
         ),
       ),
